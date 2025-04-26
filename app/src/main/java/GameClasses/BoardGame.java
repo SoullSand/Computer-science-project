3b78c25c98cf5859a035786d6307bd4e95619698
Your Name <you@example.com>
2025-04-26 17:52:32 +0300

    final commit

--- app/src/main/java/GameClasses/BoardGame.java
+++ app/src/main/java/GameClasses/BoardGame.java
@@ -138,6 +138,7 @@ public class BoardGame extends View {
         }
         if (event.getAction() == MotionEvent.ACTION_MOVE) {
 
+
         }
         if (event.getAction() == MotionEvent.ACTION_UP) {
             clickUpX = (int) event.getX();
